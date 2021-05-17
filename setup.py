27a170cc0d41f53c1823c160aab08e17414d1aa2
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2021-05-17 13:57:58 +0200

    Add Python 3 classifiers to the setup file

--- setup.py
+++ setup.py
@@ -53,6 +53,9 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Topic :: Scientific/Engineering",
         "Typing :: Typed",
     ],
