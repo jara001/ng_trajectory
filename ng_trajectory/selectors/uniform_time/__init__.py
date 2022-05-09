a6274dc935b32c03e85d0e7583ed3ed7baa7816a
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-05-09 08:08:27 +0200

    Update the selector description

--- ng_trajectory/selectors/uniform_time/__init__.py
+++ ng_trajectory/selectors/uniform_time/__init__.py
@@ -4,7 +4,9 @@
 This selector uniformly samples the input path, so
 that the points are equidistantly spaced in time.
 
-For evaluating the line, the 'profile' criterion
-is used.
+Following algorithms are used:
+- 'profile' criterion for computing the time,
+- 'cubic_spline' interpolator for smoothing the input,
+- 'uniform_distance' selector for resampling the input.
 """
 from .main import init, select
