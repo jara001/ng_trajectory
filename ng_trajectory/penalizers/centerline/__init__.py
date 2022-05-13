48afc5a58181db21f279d20fe7eb03f8a26d251e
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-05-13 10:08:00 +0200

    Add important note to Centerline penalizer

--- ng_trajectory/penalizers/centerline/__init__.py
+++ ng_trajectory/penalizers/centerline/__init__.py
@@ -11,5 +11,14 @@ Final penalty is the minimum of all of these distances.
 
 Note: Initialization of this is done only once; we expect that the
 centerline is present there (as it usually is).
+
+Important: Change the method to 'max', as default 'min' is not
+performing very well. Experimental evaluation showed that 'min'
+is 10times less successful than 'max'.
+```json
+	"penalizer_init": {
+		"method": "max"
+	}
+```
 """
 from .main import init, penalize
